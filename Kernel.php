9cfea965439dd5bce84655993d17b029b22912d5
Tavrin <etienne.doux@gmail.com>
2021-09-30 19:11:26 +0200

    ÃBeginning of setting kernel as entrypoint, refactor

--- Kernel.php
+++ Kernel.php
@@ -168,6 +168,7 @@ class Kernel
     public function bootApp(): Container
     {
         $container = $this->container = Container::getInstance();
+        $this->setRootPath();
         $this->setServices();
         return $container;
     }
